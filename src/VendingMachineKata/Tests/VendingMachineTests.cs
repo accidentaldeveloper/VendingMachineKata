5463d363545ff58a4f800ff7c63a9e59fc909a39
Michael Richardson <probackpacker@gmail.com>
2016-01-29 20:25:53 -0500

    2 tests success

--- src/VendingMachineKata/Tests/VendingMachineTests.cs
+++ src/VendingMachineKata/Tests/VendingMachineTests.cs
@@ -11,5 +11,13 @@ namespace VendingMachineKata.Tests
             var vendingMachine = new VendingMachine();
             Assert.That(vendingMachine.GetDisplay(), Is.EqualTo("INSERT COIN"));
         }
+
+        [Test]
+        public void WhenQuarterIsInsertedItDisplays25Cents()
+        {
+            var vendingMachine = new VendingMachine();
+            vendingMachine.AcceptCoin("quarter");
+            Assert.That(vendingMachine.GetDisplay(), Is.EqualTo("$0.25"));
+        }
     }
 }
--- src/VendingMachineKata/VendingMachine.cs
+++ src/VendingMachineKata/VendingMachine.cs
@@ -2,9 +2,21 @@
 {
     public class VendingMachine
     {
+        private decimal valueInserted = 0m;
+
         public string GetDisplay()
         {
-            return "INSERT COIN";
+            if (this.valueInserted == 0)
+            {
+                return "INSERT COIN";
+            }
+
+            return this.valueInserted.ToString("C");
+        }
+
+        public void AcceptCoin(string coinPhysicalDescription)
+        {
+            this.valueInserted = 0.25m;
         }
     }
 }

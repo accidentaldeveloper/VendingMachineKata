1a9e795660e1b63accfb26a8becec5d0780c9e18
Michael Richardson <probackpacker@gmail.com>
2016-01-31 22:33:31 -0500

    Test Coin creation

--- src/VendingMachineKata/Tests/CoinTests.cs
+++ src/VendingMachineKata/Tests/CoinTests.cs
@@ -8,12 +8,34 @@ namespace VendingMachineKata.Tests
     public class CoinTests
     {
         [Test]
-        public void CreateReturnsCoinWithAppropriateTypeAndValue()
+        public void CreateWithValidDescriptionReturnsCoinWithAppropriateValue()
+        {
+            TestValidCoinCreation("quarter", 0.25m );
+            TestValidCoinCreation("dime", 0.10m );
+            TestValidCoinCreation("nickel", 0.05m );
+        }
+
+        [Test]
+        public void CreateWithInvalidDescriptionReturnsFalse()
+        {
+            TestCoinCreationWithInvalidDescription("penny");
+            TestCoinCreationWithInvalidDescription("sfdeljknesv");
+        }
+
+        private static void TestCoinCreationWithInvalidDescription(string description)
         {
             ValidCoin validCoin;
-            var success = ValidCoin.TryCreate("quarter", out validCoin);
+            var success = ValidCoin.TryCreate(description, out validCoin);
+            Assert.That(success, Is.EqualTo(false));
+            Assert.That(validCoin, Is.EqualTo(null));
+        }
+
+        private static void TestValidCoinCreation(string description, decimal expectedValue)
+        {
+            ValidCoin validCoin;
+            var success = ValidCoin.TryCreate(description, out validCoin);
             Assert.That(success, Is.EqualTo(true));
-            Assert.That(validCoin.Value, Is.EqualTo(0.25m));
+            Assert.That(validCoin.Value, Is.EqualTo(expectedValue));
         }
     }
 }

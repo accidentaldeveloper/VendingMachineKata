3843609ddf1b1599f7670e1f8cd9d9fa9e3f54c9
Michael Richardson <probackpacker@gmail.com>
2016-02-01 22:04:59 -0500

    Test process of selecting product, receiving change, item is now sold out

--- src/VendingMachineKata/Tests/FullProcessTest.cs
+++ src/VendingMachineKata/Tests/FullProcessTest.cs
@@ -23,5 +23,32 @@ namespace VendingMachineKata.Tests
             vendingMachine.ReturnCoins();
             Assert.That(vendingMachine.EmptyCoinReturn(), Is.EquivalentTo(new[] { "quarter", "quarter", "quarter", "quarter" }));
         }
+
+        [Test]
+        public void CustomerIsAbleToSelectProductAndReceiveChange()
+        {
+            var vendingMachine = new VendingMachine();
+            vendingMachine.AddStock(Product.Candy);
+            vendingMachine.AcceptCoin("quarter");
+            vendingMachine.AcceptCoin("penny");
+            vendingMachine.AcceptCoin("quarter");
+            vendingMachine.AcceptCoin("dime");
+            vendingMachine.AcceptCoin("dime");
+            Assert.That(vendingMachine.GetDisplay(), Is.EqualTo("$0.70"));
+
+            vendingMachine.SelectProduct(Product.Candy);
+            Assert.That(vendingMachine.GetDisplay(), Is.EqualTo("THANK YOU"));
+            Assert.That(vendingMachine.GetDisplay(), Is.EqualTo("INSERT COIN"));
+            var change = vendingMachine.EmptyCoinReturn();
+            foreach (var coin in change)
+            {
+                vendingMachine.AcceptCoin(coin);
+            }
+
+            Assert.That(vendingMachine.GetDisplay(), Is.EqualTo("$0.05"));
+            vendingMachine.SelectProduct(Product.Candy);
+            Assert.That(vendingMachine.GetDisplay(), Is.EqualTo("SOLD OUT"));
+            Assert.That(vendingMachine.GetDisplay(), Is.EqualTo("$0.05"));
+        }
     }
 }
